8ec8e1fa39ec5fff78a876a1290d6ddd29b365a8
oblee98 <107823004+obmakesomething@users.noreply.github.com>
2026-09-02 18:03:24 +0900

    fix(kit): expose confirm dialog context for inference

--- projects/kit/components/confirm/confirm.component.ts
+++ projects/kit/components/confirm/confirm.component.ts
@@ -30,11 +30,12 @@ import {TUI_CONFIRM_OPTIONS, type TuiConfirmData} from './confirm.options';
     ],
 })
 export class TuiConfirm {
+    public readonly context =
+        injectContext<TuiDialogContext<boolean, TuiConfirmData | undefined>>();
+
     protected readonly options = inject(TUI_BUTTON_OPTIONS);
     protected readonly words = inject(TUI_CONFIRM_WORDS);
     protected readonly data = inject(TUI_CONFIRM_OPTIONS);
-    protected readonly context =
-        injectContext<TuiDialogContext<boolean, TuiConfirmData | undefined>>();
 
     protected get appearance(): string | readonly [string, string] {
         return this.context.data?.appearance || this.data.appearance || '';
